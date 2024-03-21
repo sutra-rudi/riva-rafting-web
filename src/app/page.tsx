2cc30be662c6b30ef0661b61a4942b5267964c96
sutra-rudi <rudi@sutra.hr>
2024-03-21 11:54:00 +0100

    working on more info section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import AppHeader from './components/AppHeader';
+import DodatneInformacije from './sections/DodatneInformacije';
 import styles from './styles/page.module.scss';
 export default function Home() {
   return (
     <main className={styles.homeMain}>
       <AppHeader />
+      <DodatneInformacije />
     </main>
   );
 }
---
+++ src/app/sections/DodatneInformacije.tsx
@@ -0,0 +1,49 @@
+import styles from '../styles/dodatneInformacije.module.scss';
+import imgone from '../img/imag1.png';
+import imgtwo from '../img/imag2.png';
+import imgthree from '../img/imag3.png';
+import React from 'react';
+import Image from 'next/image';
+
+const DodatneInformacije = () => {
+  return (
+    <section className={styles.dodatneInformacije}>
+      <h2 className={styles.sectionHeading}>Dodatne Informacije</h2>
+
+      <div className={styles.sectionListContainer}>
+        <div className={styles.imageBox}>
+          <div className={styles.titleHolder}>
+            <div className={styles.titleHolderInner}>
+              <h6>ZRMANJA CAMPING VILLAGE</h6>
+              <h2>SMJEŠTAJ U NAŠEM KAMPU</h2>
+            </div>
+          </div>
+
+          <Image src={imgone} fill alt='offer image' />
+        </div>
+        <div className={styles.imageBox}>
+          <div className={styles.titleHolder}>
+            <div className={styles.titleHolderInner}>
+              <h6>MIĆANOVI DVORI</h6>
+              <h2>DOMAĆA HRANA</h2>
+            </div>
+          </div>
+
+          <Image src={imgtwo} fill alt='offer image' />
+        </div>
+        <div className={styles.imageBox}>
+          <div className={styles.titleHolder}>
+            <div className={styles.titleHolderInner}>
+              <h6>OBROVAC - ZRMANJA - VELEBIT</h6>
+              <h2>ŠTO POSJETITI U OKOLICI?</h2>
+            </div>
+          </div>
+
+          <Image src={imgthree} fill alt='offer image' />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default DodatneInformacije;
