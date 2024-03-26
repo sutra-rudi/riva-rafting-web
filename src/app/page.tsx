45e94e809c96c9202815cef841dcbe5fa5d0526a
sutra-rudi <rudi@sutra.hr>
2024-03-26 15:08:44 +0100

    adding lazy loading to landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,6 @@ import DodatneInformacije from './sections/DodatneInformacije';
 import styles from './styles/page.module.scss';
 import { Suspense } from 'react';
 import Loading from './loading';
-import PogledajVideo from './sections/PogledajVideo';
-import OnamaSekcija from './sections/OnamaSekcija';
-import TripAdvisorSekcija from './sections/TripAdvisorSekcija';
-import NajpopularnijeTure from './sections/NajpopularnijeTure';
-import FAQsection from './sections/FAQsection';
-import GallerySection from './sections/GallerySection';
 import AppFooter from './components/AppFooter';
 
 export default async function Home() {
@@ -17,6 +11,13 @@ export default async function Home() {
   const PromoLazy = dynamic(() => import('./sections/PromoSekcijaJedan'), { ssr: false });
   const TureLazy = dynamic(() => import('./sections/TureSekcija'), { ssr: false });
   const TureOfferLazy = dynamic(() => import('./sections/TurePonuda'), { ssr: false });
+  const PogledajVideoLazy = dynamic(() => import('./sections/PogledajVideo'), { ssr: false });
+  const OnamaSekcijaLazy = dynamic(() => import('./sections/OnamaSekcija'), { ssr: false });
+  const TripAdvisorSekcijaLazy = dynamic(() => import('./sections/TripAdvisorSekcija'), { ssr: false });
+  const NajpopularnijeTureLazy = dynamic(() => import('./sections/NajpopularnijeTure'), { ssr: false });
+  const FAQsectionLazy = dynamic(() => import('./sections/FAQsection'), { ssr: false });
+  const GallerySectionLazy = dynamic(() => import('./sections/GallerySection'), { ssr: false });
+
   return (
     <Suspense fallback={<Loading />}>
       <main className={styles.homeMain}>
@@ -25,13 +26,13 @@ export default async function Home() {
         <PromoLazy />
         <TureLazy />
         <TureOfferLazy />
-        <PogledajVideo />
-        <OnamaSekcija />
-        <TripAdvisorSekcija />
-        <NajpopularnijeTure isLanding />
+        <PogledajVideoLazy />
+        <OnamaSekcijaLazy />
+        <TripAdvisorSekcijaLazy />
+        <NajpopularnijeTureLazy isLanding />
         <DodatneInformacije isLanding />
-        <FAQsection />
-        <GallerySection />
+        <FAQsectionLazy />
+        <GallerySectionLazy />
         <AppFooter />
       </main>
     </Suspense>
