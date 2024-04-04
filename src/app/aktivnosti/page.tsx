29c3185bf68b65f3fc8a5e74ca67c32dc9b6482a
sutra-rudi <rudi@sutra.hr>
2024-04-04 16:11:22 +0200

    deploy hotfix

--- src/app/aktivnosti/page.tsx
+++ src/app/aktivnosti/page.tsx
@@ -5,7 +5,7 @@ import styles from '../styles/page.module.scss';
 import DodatneInformacije from '../sections/DodatneInformacije';
 import NajpopularnijeTure from '../sections/NajpopularnijeTure';
 import TripAdvisorSekcija from '../sections/TripAdvisorSekcija';
-import AktivnostPrva from '../sections/AktivnostPrva';
+// import AktivnostPrva from '../sections/AktivnostPrva';
 import Loading from './loading';
 export default function Aktivnosti() {
   const mapboxApiKey = process.env.MAPBOX_API_KEY;
@@ -13,7 +13,7 @@ export default function Aktivnosti() {
     <Suspense fallback={<Loading />}>
       <main className={styles.aktivnostiMain}>
         <AppHeader />
-        <AktivnostPrva apiKey={mapboxApiKey as string} />
+        {/* <AktivnostPrva apiKey={mapboxApiKey as string} /> */}
         <DodatneInformacije isLanding={false} />
         <TripAdvisorSekcija />
         <NajpopularnijeTure isLanding={false} />
