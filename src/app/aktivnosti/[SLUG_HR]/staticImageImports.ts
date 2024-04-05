290d3f2267407916d8a5f29d14918e4f19a1b824
sutra-rudi <rudi@sutra.hr>
2024-04-05 11:05:11 +0200

    preparing image gallery

--- src/app/aktivnosti/[SLUG_HR]/staticImageImports.ts
+++ src/app/aktivnosti/[SLUG_HR]/staticImageImports.ts
@@ -1,3 +1,5 @@
+//----------------HERO SLIKE-----------//
+
 import bicikl from '../../img/aktivnosti-hero-update-slike/bicikl.png';
 import jahanje from '../../img/aktivnosti-hero-update-slike/jahanje.png';
 import jeepSafari from '../../img/aktivnosti-hero-update-slike/jeep-safari.png';
@@ -9,17 +11,283 @@ import spiljarenje from '../../img/aktivnosti-hero-update-slike/spiljarenje.png'
 import standUpPaddle from '../../img/aktivnosti-hero-update-slike/stand-up-paddle.png';
 import zrmanjaBrodom from '../../img/aktivnosti-hero-update-slike/zrmanja-brodom.png';
 
+//----------------HERO SLIKE-----------//
+
+//----------------PJESACKE TURE--------//
+
+import pjesac1 from '../../img/aktivnosti-hero-update-slike/pjesacke-ture/waking tour gallery.png';
+import pjesac2 from '../../img/aktivnosti-hero-update-slike/pjesacke-ture/waking tour gallery-1.png';
+import pjesac3 from '../../img/aktivnosti-hero-update-slike/pjesacke-ture/waking tour gallery-2.png';
+import pjesac4 from '../../img/aktivnosti-hero-update-slike/pjesacke-ture/waking tour gallery-3.png';
+import pjesac5 from '../../img/aktivnosti-hero-update-slike/pjesacke-ture/waking tour gallery-4.png';
+import pjesac6 from '../../img/aktivnosti-hero-update-slike/pjesacke-ture/waking tour gallery-5.png';
+import pjesac7 from '../../img/aktivnosti-hero-update-slike/pjesacke-ture/waking tour gallery-6.png';
+import pjesac8 from '../../img/aktivnosti-hero-update-slike/pjesacke-ture/waking tour gallery-7.png';
+import pjesac9 from '../../img/aktivnosti-hero-update-slike/pjesacke-ture/waking tour gallery-8.png';
+import pjesac10 from '../../img/aktivnosti-hero-update-slike/pjesacke-ture/waking tour gallery-9.png';
+import pjesac11 from '../../img/aktivnosti-hero-update-slike/pjesacke-ture/waking tour gallery-10.png';
+import pjesac12 from '../../img/aktivnosti-hero-update-slike/pjesacke-ture/waking tour gallery-11.png';
+import pjesac13 from '../../img/aktivnosti-hero-update-slike/pjesacke-ture/waking tour gallery-12.png';
+import pjesac14 from '../../img/aktivnosti-hero-update-slike/pjesacke-ture/waking tour gallery-13.png';
+import pjesac15 from '../../img/aktivnosti-hero-update-slike/pjesacke-ture/waking tour gallery-14.png';
+
+//----------------PJESACKE TURE--------//
+
+//----------------KAYAK TURA--------//
+
+import kayak1 from '../../img/aktivnosti-hero-update-slike/kayakTura/Kayak gallery.png';
+import kayak2 from '../../img/aktivnosti-hero-update-slike/kayakTura/Kayak gallery-1.png';
+import kayak3 from '../../img/aktivnosti-hero-update-slike/kayakTura/Kayak gallery-2.png';
+import kayak4 from '../../img/aktivnosti-hero-update-slike/kayakTura/Kayak gallery-3.png';
+
+//----------------KAYAK TURA--------//
+
+//----------------RAFTING--------//
+
+import rafting1 from '../../img/aktivnosti-hero-update-slike/rafting/Rafting gallery.png';
+import rafting2 from '../../img/aktivnosti-hero-update-slike/rafting/Rafting gallery-1.png';
+import rafting3 from '../../img/aktivnosti-hero-update-slike/rafting/Rafting gallery-2.png';
+import rafting4 from '../../img/aktivnosti-hero-update-slike/rafting/Rafting gallery-3.png';
+import rafting5 from '../../img/aktivnosti-hero-update-slike/rafting/Rafting gallery-4.png';
+import rafting6 from '../../img/aktivnosti-hero-update-slike/rafting/Rafting gallery-5.png';
+import rafting7 from '../../img/aktivnosti-hero-update-slike/rafting/Rafting gallery-6.png';
+import rafting8 from '../../img/aktivnosti-hero-update-slike/rafting/Rafting gallery-7.png';
+import rafting9 from '../../img/aktivnosti-hero-update-slike/rafting/Rafting gallery-8.png';
+import rafting10 from '../../img/aktivnosti-hero-update-slike/rafting/Rafting gallery-9.png';
+import rafting11 from '../../img/aktivnosti-hero-update-slike/rafting/Rafting gallery-10.png';
+import rafting12 from '../../img/aktivnosti-hero-update-slike/rafting/Rafting gallery-11.png';
+import rafting13 from '../../img/aktivnosti-hero-update-slike/rafting/Rafting gallery-12.png';
+import rafting14 from '../../img/aktivnosti-hero-update-slike/rafting/Rafting gallery-13.png';
+
+//----------------RAFTING--------//
+
+//----------------KAYAK ZRMANJA MORE--------//
+
+import kayakZrmanjaMore1 from '../../img/aktivnosti-hero-update-slike/kayakZrmanjaMore/kayak do mora gallery.png';
+import kayakZrmanjaMore2 from '../../img/aktivnosti-hero-update-slike/kayakZrmanjaMore/kayak do mora gallery-1.png';
+import kayakZrmanjaMore3 from '../../img/aktivnosti-hero-update-slike/kayakZrmanjaMore/kayak do mora gallery-2.png';
+import kayakZrmanjaMore4 from '../../img/aktivnosti-hero-update-slike/kayakZrmanjaMore/kayak do mora gallery-3.png';
+import kayakZrmanjaMore5 from '../../img/aktivnosti-hero-update-slike/kayakZrmanjaMore/kayak do mora gallery-4.png';
+import kayakZrmanjaMore6 from '../../img/aktivnosti-hero-update-slike/kayakZrmanjaMore/kayak do mora gallery-5.png';
+import kayakZrmanjaMore7 from '../../img/aktivnosti-hero-update-slike/kayakZrmanjaMore/kayak do mora gallery-6.png';
+import kayakZrmanjaMore8 from '../../img/aktivnosti-hero-update-slike/kayakZrmanjaMore/kayak do mora gallery-7.png';
+import kayakZrmanjaMore9 from '../../img/aktivnosti-hero-update-slike/kayakZrmanjaMore/kayak do mora gallery-8.png';
+import kayakZrmanjaMore10 from '../../img/aktivnosti-hero-update-slike/kayakZrmanjaMore/kayak do mora gallery-9.png';
+import kayakZrmanjaMore11 from '../../img/aktivnosti-hero-update-slike/kayakZrmanjaMore/kayak do mora gallery-10.png';
+import kayakZrmanjaMore12 from '../../img/aktivnosti-hero-update-slike/kayakZrmanjaMore/kayak do mora gallery-11.png';
+import kayakZrmanjaMore13 from '../../img/aktivnosti-hero-update-slike/kayakZrmanjaMore/kayak do mora gallery-12.png';
+import kayakZrmanjaMore14 from '../../img/aktivnosti-hero-update-slike/kayakZrmanjaMore/kayak do mora gallery-13.png';
+
+//----------------KAYAK ZRMANJA MORE--------//
+
+//----------------STAND UP PADDLE--------//
+
+import standupPaddle from '../../img/aktivnosti-hero-update-slike/standupPaddle/SUP gallery.png';
+import standupPaddle1 from '../../img/aktivnosti-hero-update-slike/standupPaddle/SUP gallery-1.png';
+import standupPaddle2 from '../../img/aktivnosti-hero-update-slike/standupPaddle/SUP gallery-2.png';
+import standupPaddle3 from '../../img/aktivnosti-hero-update-slike/standupPaddle/SUP gallery-3.png';
+import standupPaddle4 from '../../img/aktivnosti-hero-update-slike/standupPaddle/SUP gallery-4.png';
+import standupPaddle5 from '../../img/aktivnosti-hero-update-slike/standupPaddle/SUP gallery-5.png';
+import standupPaddle6 from '../../img/aktivnosti-hero-update-slike/standupPaddle/SUP gallery-6.png';
+import standupPaddle7 from '../../img/aktivnosti-hero-update-slike/standupPaddle/SUP gallery-7.png';
+import standupPaddle8 from '../../img/aktivnosti-hero-update-slike/standupPaddle/SUP gallery-8.png';
+import standupPaddle9 from '../../img/aktivnosti-hero-update-slike/standupPaddle/SUP gallery-9.png';
+import standupPaddle10 from '../../img/aktivnosti-hero-update-slike/standupPaddle/SUP gallery-10.png';
+import standupPaddle11 from '../../img/aktivnosti-hero-update-slike/standupPaddle/SUP gallery-11.png';
+import standupPaddle12 from '../../img/aktivnosti-hero-update-slike/standupPaddle/SUP gallery-12.png';
+import standupPaddle13 from '../../img/aktivnosti-hero-update-slike/standupPaddle/SUP gallery-13.png';
+
+//----------------STAND UP PADDLE--------//
+
+//----------------ZRMANJA BRODOM--------//
+
+import zrmanjaBrodom1 from '../../img/aktivnosti-hero-update-slike/zrmanjaBrodom/voznja brodom gallery.png';
+import zrmanjaBrodom2 from '../../img/aktivnosti-hero-update-slike/zrmanjaBrodom/voznja brodom gallery-1.png';
+import zrmanjaBrodom3 from '../../img/aktivnosti-hero-update-slike/zrmanjaBrodom/voznja brodom gallery-2.png';
+import zrmanjaBrodom4 from '../../img/aktivnosti-hero-update-slike/zrmanjaBrodom/voznja brodom gallery-3.png';
+import zrmanjaBrodom5 from '../../img/aktivnosti-hero-update-slike/zrmanjaBrodom/voznja brodom gallery-4.png';
+import zrmanjaBrodom6 from '../../img/aktivnosti-hero-update-slike/zrmanjaBrodom/voznja brodom gallery-5.png';
+import zrmanjaBrodom7 from '../../img/aktivnosti-hero-update-slike/zrmanjaBrodom/voznja brodom gallery-6.png';
+import zrmanjaBrodom8 from '../../img/aktivnosti-hero-update-slike/zrmanjaBrodom/voznja brodom gallery-7.png';
+
+//----------------ZRMANJA BRODOM--------//
+
+//----------------JEEP SAFARI--------//
+
+import jeepSafari1 from '../../img/aktivnosti-hero-update-slike/jeep/Jeep tpur gallery.png';
+import jeepSafari2 from '../../img/aktivnosti-hero-update-slike/jeep/Jeep tpur gallery-1.png';
+import jeepSafari3 from '../../img/aktivnosti-hero-update-slike/jeep/Jeep tpur gallery-2.png';
+import jeepSafari4 from '../../img/aktivnosti-hero-update-slike/jeep/Jeep tpur gallery-3.png';
+import jeepSafari5 from '../../img/aktivnosti-hero-update-slike/jeep/Jeep tpur gallery-4.png';
+import jeepSafari6 from '../../img/aktivnosti-hero-update-slike/jeep/Jeep tpur gallery-5.png';
+import jeepSafari7 from '../../img/aktivnosti-hero-update-slike/jeep/Jeep tpur gallery-6.png';
+import jeepSafari8 from '../../img/aktivnosti-hero-update-slike/jeep/Jeep tpur gallery-7.png';
+import jeepSafari9 from '../../img/aktivnosti-hero-update-slike/jeep/Jeep tpur gallery-8.png';
+
+//----------------JEEP SAFARI--------//
+
+//----------------SPILJARENJE--------//
+
+import spiljarenje1 from '../../img/aktivnosti-hero-update-slike/spiljarenje/spiljarenje gallery.png';
+import spiljarenje2 from '../../img/aktivnosti-hero-update-slike/spiljarenje/spiljarenje gallery-1.png';
+import spiljarenje3 from '../../img/aktivnosti-hero-update-slike/spiljarenje/spiljarenje gallery-2.png';
+import spiljarenje4 from '../../img/aktivnosti-hero-update-slike/spiljarenje/spiljarenje gallery-3.png';
+import spiljarenje5 from '../../img/aktivnosti-hero-update-slike/spiljarenje/spiljarenje gallery-4.png';
+import spiljarenje6 from '../../img/aktivnosti-hero-update-slike/spiljarenje/spiljarenje gallery-5.png';
+import spiljarenje7 from '../../img/aktivnosti-hero-update-slike/spiljarenje/spiljarenje gallery-6.png';
+import spiljarenje8 from '../../img/aktivnosti-hero-update-slike/spiljarenje/spiljarenje gallery-7.png';
+
+//----------------SPILJARENJE--------//
+
+//----------------JAHANJE--------//
+
+import jahanje1 from '../../img/aktivnosti-hero-update-slike/jahanje/jahanje gallery.png';
+import jahanje2 from '../../img/aktivnosti-hero-update-slike/jahanje/jahanje gallery-1.png';
+import jahanje3 from '../../img/aktivnosti-hero-update-slike/jahanje/jahanje gallery-2.png';
+import jahanje4 from '../../img/aktivnosti-hero-update-slike/jahanje/jahanje gallery-3.png';
+import jahanje5 from '../../img/aktivnosti-hero-update-slike/jahanje/jahanje gallery-4.png';
+import jahanje6 from '../../img/aktivnosti-hero-update-slike/jahanje/jahanje gallery-5.png';
+
+//----------------JAHANJE--------//
+
+//----------------BICIKL--------//
+
+import bicikl1 from '../../img/aktivnosti-hero-update-slike/bicikl(1).png';
+
+//----------------BICIKL--------//
+
 const AktivnostiHeroSlike = [
-  { aktivnostId: 1, aktivnostHeroUrl: pjesackeTure, aktivnostGalerija: ['', '', ''] },
-  { aktivnostId: 2, aktivnostHeroUrl: kayakTura, aktivnostGalerija: ['', '', ''] },
-  { aktivnostId: 3, aktivnostHeroUrl: rafting, aktivnostGalerija: ['', '', ''] },
-  { aktivnostId: 4, aktivnostHeroUrl: kayakZrmanjaMore, aktivnostGalerija: ['', '', ''] },
-  { aktivnostId: 5, aktivnostHeroUrl: standUpPaddle, aktivnostGalerija: ['', '', ''] },
-  { aktivnostId: 6, aktivnostHeroUrl: zrmanjaBrodom, aktivnostGalerija: ['', '', ''] },
-  { aktivnostId: 7, aktivnostHeroUrl: jeepSafari, aktivnostGalerija: ['', '', ''] },
-  { aktivnostId: 8, aktivnostHeroUrl: spiljarenje, aktivnostGalerija: ['', '', ''] },
-  { aktivnostId: 9, aktivnostHeroUrl: jahanje, aktivnostGalerija: ['', '', ''] },
-  { aktivnostId: 10, aktivnostHeroUrl: bicikl, aktivnostGalerija: ['', '', ''] },
+  {
+    aktivnostId: 1,
+    aktivnostHeroUrl: pjesackeTure,
+    aktivnostGalerija: [
+      pjesac1,
+      pjesac2,
+      pjesac3,
+      pjesac4,
+      pjesac5,
+      pjesac6,
+      pjesac7,
+      pjesac8,
+      pjesac9,
+      pjesac10,
+      pjesac11,
+      pjesac12,
+      pjesac13,
+      pjesac14,
+      pjesac15,
+    ],
+  },
+  { aktivnostId: 2, aktivnostHeroUrl: kayakTura, aktivnostGalerija: [kayak1, kayak2, kayak3, kayak4] },
+  {
+    aktivnostId: 3,
+    aktivnostHeroUrl: rafting,
+    aktivnostGalerija: [
+      rafting1,
+      rafting2,
+      rafting3,
+      rafting4,
+      rafting5,
+      rafting6,
+      rafting7,
+      rafting8,
+      rafting9,
+      rafting10,
+      rafting11,
+      rafting12,
+      rafting13,
+      rafting14,
+    ],
+  },
+  {
+    aktivnostId: 4,
+    aktivnostHeroUrl: kayakZrmanjaMore,
+    aktivnostGalerija: [
+      kayakZrmanjaMore1,
+      kayakZrmanjaMore2,
+      kayakZrmanjaMore3,
+      kayakZrmanjaMore4,
+      kayakZrmanjaMore5,
+      kayakZrmanjaMore6,
+      kayakZrmanjaMore7,
+      kayakZrmanjaMore8,
+      kayakZrmanjaMore9,
+      kayakZrmanjaMore10,
+      kayakZrmanjaMore11,
+      kayakZrmanjaMore12,
+      kayakZrmanjaMore13,
+      kayakZrmanjaMore14,
+    ],
+  },
+  {
+    aktivnostId: 5,
+    aktivnostHeroUrl: standUpPaddle,
+    aktivnostGalerija: [
+      standupPaddle,
+      standupPaddle1,
+      standupPaddle2,
+      standupPaddle3,
+      standupPaddle4,
+      standupPaddle5,
+      standupPaddle6,
+      standupPaddle7,
+      standupPaddle8,
+      standupPaddle9,
+      standupPaddle10,
+      standupPaddle11,
+      standupPaddle12,
+      standupPaddle13,
+    ],
+  },
+  {
+    aktivnostId: 6,
+    aktivnostHeroUrl: zrmanjaBrodom,
+    aktivnostGalerija: [
+      zrmanjaBrodom1,
+      zrmanjaBrodom2,
+      zrmanjaBrodom3,
+      zrmanjaBrodom4,
+      zrmanjaBrodom5,
+      zrmanjaBrodom6,
+      zrmanjaBrodom7,
+      zrmanjaBrodom8,
+    ],
+  },
+  {
+    aktivnostId: 7,
+    aktivnostHeroUrl: jeepSafari,
+    aktivnostGalerija: [
+      jeepSafari1,
+      jeepSafari2,
+      jeepSafari3,
+      jeepSafari4,
+      jeepSafari5,
+      jeepSafari6,
+      jeepSafari7,
+      jeepSafari8,
+      jeepSafari9,
+    ],
+  },
+  {
+    aktivnostId: 8,
+    aktivnostHeroUrl: spiljarenje,
+    aktivnostGalerija: [
+      spiljarenje1,
+      spiljarenje2,
+      spiljarenje3,
+      spiljarenje4,
+      spiljarenje5,
+      spiljarenje6,
+      spiljarenje7,
+      spiljarenje8,
+    ],
+  },
+  {
+    aktivnostId: 9,
+    aktivnostHeroUrl: jahanje,
+    aktivnostGalerija: [jahanje1, jahanje2, jahanje3, jahanje4, jahanje5, jahanje6],
+  },
+  { aktivnostId: 10, aktivnostHeroUrl: bicikl, aktivnostGalerija: [bicikl1] },
 ];
 
 export default AktivnostiHeroSlike;
