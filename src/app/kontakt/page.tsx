91482becd0a922edd0dd39a44dcd047101756915
sutra-rudi <rudi@sutra.hr>
2024-04-17 12:33:51 +0200

    deploy env fix

--- src/app/kontakt/page.tsx
+++ src/app/kontakt/page.tsx
@@ -3,19 +3,20 @@ import styles from '../styles/contact.module.scss';
 import AppFooter from '../components/AppFooter';
 import kontaktHero from '../img/kontakt-slike/kontaktHero.png';
 import AppHeader from '../components/AppHeader';
-import PageContent from './PageContent';
 import Loading from './loading';
 import FAQsection from '../sections/FAQsection';
+import dynamic from 'next/dynamic';
 
 export default async function Kontakt() {
+  const LazyContent = dynamic(() => import('./PageContent'));
   return (
-    <Suspense fallback={<Loading />}>
+    <main className={styles.sectionMain}>
       <AppHeader />
-      <main className={styles.sectionMain}>
-        <PageContent title={`KONTAKTIRAJTE NAS I\nREZERVIRAJTE SVOJ TERMIN`} imgSrc={kontaktHero} />
-        <FAQsection />
-        <AppFooter />
-      </main>
-    </Suspense>
+      <Suspense fallback={<Loading />}>
+        <LazyContent title={`KONTAKTIRAJTE NAS I\nREZERVIRAJTE SVOJ TERMIN`} imgSrc={kontaktHero} />
+      </Suspense>
+      <FAQsection />
+      <AppFooter />
+    </main>
   );
 }
