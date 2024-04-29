ea4cacf7d2fe67feba824b2c07adf68089d56e85
sutra-rudi <rudi@sutra.hr>
2024-04-29 12:17:32 +0200

    render update

--- src/app/components/AppHeader.tsx
+++ src/app/components/AppHeader.tsx
@@ -2,7 +2,7 @@
 
 import Link from 'next/link';
 import React from 'react';
-import svgAppLogo from '../img/APP-LOGO-FOOTER.svg';
+import svgAppLogo from '../img/app-logo-master.svg';
 import Image from 'next/image';
 
 import styles from '../styles/appHeader.module.scss';
