0986fb88a2b90fb48ddf6f2d28c0421a67ba30e1
sutra-rudi <rudi@sutra.hr>
2024-06-07 10:06:29 +0200

    og update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,43 +16,12 @@ export const viewport: Viewport = {
   themeColor: '#eeeeee',
 };
 
-// export const metadata: Metadata = {
-//   title: {
-//     default: 'Riva Rafting - Rafting Adventures in Croatia',
-//     template: '%s | Riva Rafting',
-//   },
-//   generator: `Next.js`,
-//   metadataBase: new URL(`https://riva-rafting-web-under-construction.vercel.app`),
-//   openGraph: {
-//     type: 'website',
-//     title: 'Riva Rafting',
-//     description: 'Experience thrilling rafting adventures in Croatia with Riva Rafting.',
-//     siteName: 'Riva Rafting',
-//     url: new URL(`https://riva-rafting-web-under-construction.vercel.app`),
-//     locale: 'hr',
-//     images: [''],
-//   },
-//   description:
-//     'Experience thrilling rafting adventures in Croatia with Riva Rafting. Discover the beauty of Croatian rivers and nature while enjoying adrenaline-pumping rafting experiences.',
-//   creator: 'Riva Rafting',
-//   publisher: 'Vercel',
-//   keywords: 'rafting, Croatia, adventure, adrenaline, rivers, nature, water sports, outdoor activities',
-//   robots: {
-//     index: true,
-//     follow: true,
-//     nocache: true,
-//   },
-//   twitter: {
-//     description:
-//       'Experience thrilling rafting adventures in Croatia with Riva Rafting. Discover the beauty of Croatian rivers and nature while enjoying adrenaline-pumping rafting experiences.',
-//     creator: 'Riva Rafting',
-//   },
-//   category: 'Rafting',
-//   authors: [
-//     { name: 'Riva Rafting', url: new URL(`https://riva-rafting-web-under-construction.vercel.app`) },
-//     { name: 'Studio Sutra', url: new URL(`https://www.sutra.hr/`) },
-//   ],
-// };
+export const metadata: Metadata = {
+  title: {
+    default: 'Riva Rafting',
+    template: '%s | Riva Rafting',
+  },
+};
 
 export default function RootLayout({
   children,
